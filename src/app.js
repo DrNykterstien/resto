81cda761eb711682040ad617e9195d1e1a3b62a1
DrNykterstien <abdullanasser.dev@hotmail.com>
2022-05-20 16:07:00 +0200

    :star: feat[UI]: uploaded ejs and styles files

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const expressLayouts = require("express-ejs-layouts");
 require('dotenv').config();
 
 const app = express();
 
 app.disable('x-powered-by');
 app.use(express.static('public'));
+
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/website");
+app.set('views', './src/views')
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
