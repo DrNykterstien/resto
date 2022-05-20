daf8da03c6318234f4548ea854c06d13797f6518
DrNykterstien <abdullanasser.dev@hotmail.com>
2022-05-20 22:30:07 +0200

    :star: feat[website]: handle success or failure of createTableReservation

--- src/controllers/website.controller.js
+++ src/controllers/website.controller.js
@@ -8,11 +8,18 @@ const getHomeRenderingController = async (req, res) => {
 
 const createTableReservationRenderingController = async (req, res) => {
   const input = schemaValidation(createTableReservationSchema, req.body);
-  if (input.code === 400) return res.render('pages/website/home');
+  if (input.code === 400)
+    return res.render('pages/empty', {
+      message: input.message,
+      layout: './layouts/failure-website'
+    });
   const data = await createTableReservation(input);
   if (data.code === 400 || data.code === 500)
-    return res.render('pages/website/home');
-  return res.render('pages/website/home');
+    return res.render('pages/empty', {
+      message: data.message,
+      layout: './layouts/failure-website'
+    });
+  return res.render('pages/empty', { layout: './layouts/success-website' });
 };
 
 module.exports = {
